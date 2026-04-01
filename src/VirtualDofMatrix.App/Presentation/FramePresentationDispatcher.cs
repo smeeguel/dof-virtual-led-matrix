6b8201b783805e3d684a6d88af59134c080a547d
Miguel Molinari <smeeguel@gmail.com>
2026-04-01 04:53:47 -0400

    Coalesce frame dispatches to prevent UI queue starvation

--- src/VirtualDofMatrix.App/Presentation/FramePresentationDispatcher.cs
+++ src/VirtualDofMatrix.App/Presentation/FramePresentationDispatcher.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System.Windows.Threading;
 using VirtualDofMatrix.App.Serial;
 using VirtualDofMatrix.Core;
@@ -7,7 +8,11 @@ namespace VirtualDofMatrix.App.Presentation;
 public sealed class FramePresentationDispatcher : IDisposable
 {
     private readonly Dispatcher _dispatcher;
+    private readonly object _gate = new();
     private SerialEmulatorHost? _host;
+    private FramePresentation _latestFrame = new(Array.Empty<byte>(), 0, 0, 0, DateTimeOffset.UnixEpoch);
+    private bool _hasFrame;
+    private int _uiDispatchScheduled;
 
     public FramePresentationDispatcher(Dispatcher dispatcher)
     {
@@ -29,7 +34,37 @@ public sealed class FramePresentationDispatcher : IDisposable
 
     private void OnFramePresentedFromHost(object? sender, FramePresentation frame)
     {
-        _dispatcher.BeginInvoke(() => FramePresentedOnUiThread?.Invoke(this, frame));
+        lock (_gate)
+        {
+            _latestFrame = frame;
+            _hasFrame = true;
+        }
+
+        if (Interlocked.Exchange(ref _uiDispatchScheduled, 1) == 0)
+        {
+            _dispatcher.BeginInvoke(DispatchLatestFrameToUi);
+        }
+    }
+
+    private void DispatchLatestFrameToUi()
+    {
+        while (true)
+        {
+            FramePresentation frame;
+            lock (_gate)
+            {
+                if (!_hasFrame)
+                {
+                    Interlocked.Exchange(ref _uiDispatchScheduled, 0);
+                    return;
+                }
+
+                frame = _latestFrame;
+                _hasFrame = false;
+            }
+
+            FramePresentedOnUiThread?.Invoke(this, frame);
+        }
     }
 
     public void Dispose()
