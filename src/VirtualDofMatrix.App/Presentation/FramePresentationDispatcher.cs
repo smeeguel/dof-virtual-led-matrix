a0eac4acccb25cea3d161f4b4eab9293ac0c13ac
Miguel Molinari <smeeguel@gmail.com>
2026-03-31 23:39:14 -0400

    Coalesce UI frame dispatch to avoid render backlog

--- src/VirtualDofMatrix.App/Presentation/FramePresentationDispatcher.cs
+++ src/VirtualDofMatrix.App/Presentation/FramePresentationDispatcher.cs
@@ -8,6 +8,8 @@ public sealed class FramePresentationDispatcher : IDisposable
 {
     private readonly Dispatcher _dispatcher;
     private SerialEmulatorHost? _host;
+    private FramePresentation? _latestFrame;
+    private int _dispatchQueued;
 
     public FramePresentationDispatcher(Dispatcher dispatcher)
     {
@@ -29,7 +31,29 @@ public sealed class FramePresentationDispatcher : IDisposable
 
     private void OnFramePresentedFromHost(object? sender, FramePresentation frame)
     {
-        _dispatcher.BeginInvoke(() => FramePresentedOnUiThread?.Invoke(this, frame));
+        Interlocked.Exchange(ref _latestFrame, frame);
+
+        if (Interlocked.CompareExchange(ref _dispatchQueued, 1, 0) == 0)
+        {
+            _dispatcher.BeginInvoke(DispatcherPriority.Render, new Action(DispatchLatestFrame));
+        }
+    }
+
+    private void DispatchLatestFrame()
+    {
+        var frame = Interlocked.Exchange(ref _latestFrame, null);
+        if (frame is not null)
+        {
+            FramePresentedOnUiThread?.Invoke(this, frame);
+        }
+
+        Interlocked.Exchange(ref _dispatchQueued, 0);
+
+        if (Volatile.Read(ref _latestFrame) is not null &&
+            Interlocked.CompareExchange(ref _dispatchQueued, 1, 0) == 0)
+        {
+            _dispatcher.BeginInvoke(DispatcherPriority.Render, new Action(DispatchLatestFrame));
+        }
     }
 
     public void Dispose()
@@ -39,5 +63,8 @@ public sealed class FramePresentationDispatcher : IDisposable
             _host.FramePresented -= OnFramePresentedFromHost;
             _host = null;
         }
+
+        Interlocked.Exchange(ref _latestFrame, null);
+        Interlocked.Exchange(ref _dispatchQueued, 0);
     }
 }
